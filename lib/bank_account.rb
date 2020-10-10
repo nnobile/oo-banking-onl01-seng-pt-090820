7a16b5d6d350771077fa29fddae436277c464915
Nick Nobile <nickcnobile@gmail.com>
2020-10-10 23:17:55 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,47 @@
+require "pry"
+
 class BankAccount
+  attr_accessor :balance, :status, :transfer
+  attr_reader :name
+
+  def initialize(name, status = "open")
+    @name = name
+    @balance = 1000
+    @status = status
+  end
+
+  def deposit(money)
+    self.balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  # def valid?
+  #   # if @balance == 0
+  #   #   false
+  #   # elsif @status == "closed"
+  #   #   false
+  #   # else
+  #   #   true
+  #   # end
+  # end
+
+  def valid?
+    if self.balance > 0 && self.status == "open"
+      true
+    else
+      false
+  end
+end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
+
+
+# review self and understanding of it
+# make sure to call methods appropriately. it is either instance.method or class.method. we are always calling methods on something.
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, status = "pending", amount)
+    # @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if @status != "complete" && sender.valid?
+      @receiver.balance = @receiver.balance + amount
+      @sender.balance = @sender.balance - amount
+      @status = "complete"
+    elsif
+      sender.balance < amount
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.execute_transaction && valid? && @receiver.balance > amount && status == "complete"
+      @receiver.balance = @receiver.balance - amount
+      @sender.balance = @sender.balance + amount
+      @status = "reversed"
+    end
+  end
 end
