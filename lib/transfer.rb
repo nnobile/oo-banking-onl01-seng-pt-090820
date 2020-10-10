e460310373b7cc3d4376f4f96247ccc037ae6a42
Nick Nobile <nickcnobile@gmail.com>
2020-10-10 23:32:41 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Transfer
   attr_accessor :sender, :receiver, :status, :amount
 
@@ -13,23 +15,43 @@ class Transfer
     sender.valid? && receiver.valid?
   end
 
+  # def execute_transaction
+  #   if @status != "complete" && sender.valid?
+  #     @receiver.balance = @receiver.balance + amount
+  #     @sender.balance = @sender.balance - amount
+  #     @status = "complete"
+  #   elsif
+  #     sender.balance < amount
+  #     @status = "rejected"
+  #     "Transaction rejected. Please check your account balance."
+  #   end
+  # end
+
   def execute_transaction
-    if @status != "complete" && sender.valid?
-      @receiver.balance = @receiver.balance + amount
-      @sender.balance = @sender.balance - amount
-      @status = "complete"
-    elsif
-      sender.balance < amount
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      if @status != "complete" && valid? && sender.balance > amount
+        @receiver.balance = @receiver.balance + amount
+        @sender.balance = @sender.balance - amount
+        @status = "complete"
+      else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
     end
-  end
+
+  # def reverse_transfer
+  #   if self.execute_transaction && valid? && @receiver.balance > amount && status == "complete"
+  #     @receiver.balance = @receiver.balance - amount
+  #     @sender.balance = @sender.balance + amount
+  #     @status = "reversed"
+  #   end
 
   def reverse_transfer
-    if self.execute_transaction && valid? && @receiver.balance > amount && status == "complete"
+    if valid? && @receiver.balance > amount && status == "complete"
+      binding.pry
       @receiver.balance = @receiver.balance - amount
       @sender.balance = @sender.balance + amount
       @status = "reversed"
     end
   end
+
 end
